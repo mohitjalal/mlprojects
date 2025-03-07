340186f79635af450f8e8130a25b869ef02c8efe
mohit jalal <mohitjalal9@gmail.com>
2025-03-07 20:25:46 +0530

    Addind src file

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE="{datetime.now().strftime('%Y-%m-%d')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+)
+
+if __name__=="__main__":
+    logging.info("Logging has started")
